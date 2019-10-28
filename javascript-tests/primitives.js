a5259fe4b2a0a603b7e694fb1faecffd15d035bc
Zander Schock <zander.schock@bellevuecollege.edu>
2019-10-28 12:19:46 -0700

    in class java

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,30 @@
+const myString = "Learning Javascript in class.";
+const myBoolean = true;
+const myFirstNumericaVariable = 3;
+const myNull = null;
+let myUndefined;
+
+myUndefined = "learning more Javascript in class";
+
+var oldStyle = "This is the way we used to do it.";
+let newChangeableStyly = "This allows you to delcare a variable that can be changes.";
+const constantVariable = "This variable can not be changed. Except...";
+
+const quotable = 'he said,"Hello". That\'s all folks.';
+
+console.log(myString);
+console.log(typeof myString);
+console.log(myFirstNumericaVariable);
+console.log(typeof myFirstNumericaVariable);
+console.log(typeof myBoolean);
+console.log(myNull);
+console.log('TYPE:', typeof myUndefined);
+console.log('Add 3 + 25', myFirstNumericaVariable + 25);
+console.log('add undefined:', myUndefined + 25);
+
+
+
+
+
+
+
