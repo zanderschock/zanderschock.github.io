852af0fb1ec8bce17530d4f3d083c4f9031698b6
Zander Schock <zander.schock@bellevuecollege.edu>
2019-11-06 12:12:18 -0800

    in class

---
+++ for-loops/index.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    const listButton = document.getElementById('run-for-loop');
+
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+            // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i. 
+            // The loop should iterate 5 times (count from zero to five).
+            elfCode.appendToList(myList, i);
+            // END LOOP
+    }
+}                    
+        
