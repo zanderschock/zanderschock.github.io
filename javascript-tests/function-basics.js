e80152d0279be46b86a11de5550495700eef9040
zanderschock <55711812+zanderschock@users.noreply.github.com>
2019-11-10 22:02:42 -0800

    done

---
+++ javascript-tests/function-basics.js
@@ -0,0 +1,35 @@
+const simpleFunction
+console.log(simpleFunction);
+const functionParameters
+console.log(functionParemeters);
+const functionReturn
+console.log(functionReturn);
+
+window.onload = () => {
+    // const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+   // const functionCallsAction = document.getElementById('functionCallsAction');
+       
+   
+function simple() {
+    console.log('simple');
+    simpleFunctionDisplay.textContent = "simple";
+}
+}
+functionCallsAction.onclick = simple;
+
+const data = nameDisplay.value;                
+
+const elfCode = {
+    appendToList: (list, value) => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(value));
+        list.appendChild(li);
+    }
+}
+const someAction = document.getElementById('someAction');
+someAction.onclick = function() {
+    // Handle the onclick event in here.
+}
+
+
+       
