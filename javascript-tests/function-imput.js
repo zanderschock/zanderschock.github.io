7497767532858c46721409694eec078e42fa4e83
Zander Schock <zander.schock@bellevuecollege.edu>
2019-11-20 11:35:48 -0800

    fixs

--- javascript-tests/function-imput.js
+++ javascript-tests/function-imput.js
@@ -1,60 +1,71 @@
-window.onload = () => {
-    const feetToInchesAction = 
-        document.getElementById('feetToInchesAction');
+window.onload= ()=> {
 
-    feetToInchesAction.onclick = () => {
-        const feetToInchesInput = document.getElementById('feetToInchesInput');
-        const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
+const feetToInchesAction = document.getElementById('feetToInchesAction');
+ const milesToFeetAction=document.getElementById('milesToFeetAction');
+ const areaOfTriangleAction=document.getElementById('areaOfTriangleAction');
+ const areaOfCircleAction=document.getElementById('areaOfCircleAction')
 
-        feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
-    };
 
-const feetToInches = (feet) => {
-    return feet * 12;
+
+feetToInchesAction.onclick = () => {
+const feetToInchesInput = document.getElementById('feetToInchesInput');
+const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
+ feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
 };
 
-    const milesToFeetAction = 
-        document.getElementById('milesToFeetAction');
 
-    milesToFeetAction.onclick = () => {
-        const milesToFeetInput = document.getElementById('milesToFeetInput');
-        const milesToFeetDisplay = document.getElementById('milesToFeetDisplay');
 
-        milesToFeetDisplay.textContent = milesToFeet(milesToFeetInput.value);
-    };
+milesToFeetAction.onclick = () => {
+ const milesToFeetInput=document.getElementById('milesToFeetInput');
+
+ const milesToFeetIDisplay=document.getElementById('milesToFeetDisplay');
+
+ milesToFeetIDisplay.textContent=milesToFeet(milesToFeetInput.value)
+}
+
+
+
+areaOfTriangleAction.onclick =() => {
+ const areaOfTriangleInputBase=document.getElementById('areaOfTriangleInputBase');
+
+const areaOfTriangleInputHeight=document.getElementById('areaOfTriangleInputHeight');
+
+const areaOfTriangleDisplay=document.getElementById("areaOfTriangleDisplay");
+
+areaOfTriangleDisplay.textContent=areaOfTriangle(areaOfTriangleInputBase.value, areaOfTriangleInputHeight.value);
+}
+
+
+
+areaOfCircleAction.onclick= () => {
+const areaOfCircleInput=document.getElementById('areaOfCircleInput');
+
+const areaOfCircleDisplay=document.getElementById('areaOfCircleDisplay');
+    
+areaOfCircleDisplay.textContent=areaOfCircle(areaOfCircleInput.value);
+}
+
+
+ const feetToInches = (feet) => {
+    return feet *12;
+    }
+
+
 
 const milesToFeet = (miles) => {
-    return miles * 5280;
-};
-
-    const areaOfTriangle = 
-     document.getElementById('areaOfTriangleAction');
-
-    areaOfTriangleAction.onclick = () => {
-    const areaOfTriangleInputBase=document.getElementById('areaOfTriangleInputBase');
-    const areaOfTriangleInputHeight=document.getElementById('areaOfTriangleInputHeight');
-    const areaOfTriangleInput = document.getElementById('areaOfTriangleInput');
-    const areaOfTriangleDisplay = document.getElementById('areaOfTriangleDisplay');
+ return miles*5280;
+    }
 
 
 
-    areaOfTriangleDisplay.textContent = areaOfTriangle(baseAreaOfTriangleInput.value, heightAreaOfTriangleInput.value);
-    };
+const areaOfTriangle =(base, height) => {
+    return (base * height/2);
+    }
 
-const areaOfTriangle = (base, height) => {
-    return base * height / 2;
-};
 
-    const areaOfCircleAction = 
-    document.getElementById('areaOfCircleAction');
 
-    areaOfCircleAction.onclick = () => {
-    const areaOfCircleInput = document.getElementById('areaOfCircleInput');
-    const areaOfCircleDisplay = document.getElementById('areaOfCircleDisplay');
-    areaOfCircleDisplay.textContent = areaOfCircle(areaOfCircleInput.value);
- };
+const areaOfCircle =(radius) => {
+    return Math.PI * Math.pow(radius,2);
+    }
 
-const areaOfCircle = (radius) => {
-return radius * radius * Math.PI;
-};
-};
+}
